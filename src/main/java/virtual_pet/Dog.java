b22452b9c5930daa8b1f1161f4cc5120febcc188
DeJuanSpencer <dejuanspencer@gmail.com>
2022-01-20 13:43:12 -0500

    Added new dog object so different objects can be created.

--- src/main/java/virtual_pet/Dog.java
+++ src/main/java/virtual_pet/Dog.java
@@ -10,6 +10,7 @@ public class Dog {
     public String greeting;
     public int tick;
     boolean isAlive = true;
+    boolean wantsToStayWithYou = true;
 
 
     public Dog(String name, String furColor, String favoriteToy, String favoriteFood, int age, int weight) {
@@ -20,27 +21,6 @@ public class Dog {
         this.weight = weight;
         this.favoriteFood = favoriteFood;
     }
-
-    public Dog() {
-
-    }
-
-    public int tick() {
-        int time = 30;
-        return time;
-    }
-
-    public boolean play(){
-
-        boolean result = false;
-        return result;
-    }
-
-    public boolean hunger(){
-        boolean result = false;
-        return result;
-    }
-
 
 
     public String getName() {
@@ -63,17 +43,58 @@ public class Dog {
         return weight;
     }
 
-    public void greeting(){
-        System.out.println("My name is " + name);
-        System.out.println("My favorite toy(s) is/are " +favoriteToy);
-        System.out.println("My favorite food is " + favoriteFood);
 
+    public int tick() {
+        int time = 30;
+        return time;
+    }
+
+    public boolean play() {
+
+     boolean result = false;
+        return result;
+    }
+
+    public boolean hunger() {
+        boolean result = false;
+        if (result) {
+            System.out.println("I am hungry");
+        } else if (!result) {
+            System.out.println("I am not hungry");
+        }
+        return result;
+    }
+
+    public boolean thirst() {
+        boolean result = false;
+        if (result) {
+            System.out.println("I am thirsty");
+        } else if (!result) {
+            System.out.println("I am not thirsty");
+        }
+        return result;
+    }
+
+    public void stats() {
+        System.out.println("My name is " + name);
+        boolean hungerStatus = hunger();
+        boolean thirstStatus = thirst();
+        System.out.println("My name is " + name);
+        System.out.println("My name is " + name);
+        System.out.println("My name is " + name);
 
     }
 
 
+    public String greeting() {
+        String hello = "";
 
-
-
-
+        hello += "My name is " + name + ". ";
+        hello+= "I am " + getAge() + "years old. ";
+        hello+= "I am " + getWeight() + "lbs. ";
+        hello += "My favorite toy(s) is/are " + favoriteToy + ". ";
+        hello += "My favorite food is " + favoriteFood + ".";
+        
+        return hello;
+    }
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,24 +1,40 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
+
 public class VirtualPetApplication {
 
-private final Dog marchi = new Dog("Marchi", "brown", "squirrels", "steak",4, 45);
+    private Dog marchi = new Dog("Marchi", "brown", "squirrels", "steak", 4, 45);
 
     public static void main(String[] args) {
-       VirtualPetApplication myMethod = new VirtualPetApplication();
-
-
-       myMethod.dogTestMethod();
+        VirtualPetApplication myGame = new VirtualPetApplication();
+        myGame.gameMethod();
     }
 
-    public void dogTestMethod(){
-        marchi.greeting();
+    public void gameMethod() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name of your pet");
+        String name = input.nextLine();
+        System.out.println("Fur Color?");
+        String furColor = input.nextLine();
+        System.out.println("Favorite toy?");
+        String favoriteToy = input.nextLine();
+        System.out.println("Favorite food?");
+        String favoriteFood = input.nextLine();
+        System.out.println("Age?");
+        int age = input.nextInt();
+        System.out.println("Weight?");
+        int weight = input.nextInt();
+        input.nextLine();
+
+        Dog doggo = new Dog(name,furColor,favoriteToy,favoriteFood,age,weight);
+        System.out.println(doggo.greeting());
+
     }
-
-    public boolean alive(){
-return marchi.isAlive;
-    }
-
-
-
 }
+
+
+
+
+
