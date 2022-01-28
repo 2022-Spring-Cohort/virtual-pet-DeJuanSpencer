ad7af56a0b43952d7a221cb27c0e30fa6d4adfd9
DeJuanSpencer <dejuanspencer@gmail.com>
2022-01-28 15:01:35 -0500

    Made adjustments to the admitting and getting to/from shelter.

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -21,26 +21,38 @@ public class VirtualPetApplication {
         VirtualPet pet = pets.get(rand.nextInt(pets.size()));
 
 
-
 //TODO Add something to interact with the pet if it needs something
-        
+
         while (pet.isAlive) {
             int choice = -1;
-            while(choice!=0 &&  choice!=1){
+            while (choice != 1 && choice != 2) {
                 System.out.println("Would you like to adopt a pet or place one into the shelter?");
                 System.out.println("Press 1 for adopt or press 2 to place pet into the shelter");
                 choice = input.nextInt();
-                yourPetsInGame.add(interactWithShelter.getFromShelter());
-                
-            }
-            if(choice==1){
-                System.out.println("Oh yay! Someone is going to have a very nice home!");
-                interactWithShelter.getFromShelter();
+
 
             }
-            else if(choice==2){
+            if (choice == 1) {
+                System.out.println("Oh yay! Someone is going to have a very nice home!");
+                System.out.println("Here are the pets you have so far...");
+
+
+                yourPetsInGame.add(interactWithShelter.getFromShelter());
+
+
+            } else if (choice == 2) {
                 System.out.println("No need to worry about a thing. All animals are our family! ^_^");
-                interactWithShelter.admitToShelter();
+
+                int count = 0;
+                for (VirtualPet aPet : yourPetsInGame) {
+                    System.out.printf("%d: %s", count, yourPetsInGame.get(count));
+                    count++;
+                }
+                System.out.println("Choose the number associated with your pet to surrender it I mean... to give it a new home...");
+                int decision = input.nextInt();
+                input.nextLine();
+                interactWithShelter.admitToShelter(yourPetsInGame.get(decision));
+                yourPetsInGame.remove(decision);
             }
 //TODO This looks a bit off. Consider adjustments
             if (pet.hungerLevel >= 5 || pet.thirstLevel >= 5) {
