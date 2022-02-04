3a7d16e97b73c3cde4695c38e196d234f6e63a0a
DeJuanSpencer <dejuanspencer@gmail.com>
2022-02-04 14:02:43 -0500

    Started on iteration 3. Created abstract class of robot animals.

---
+++ src/main/java/virtual_pet/VirtualRobotPet.java
@@ -0,0 +1,82 @@
+package virtual_pet;
+
+public abstract class VirtualRobotPet {
+    private String name;
+    private String animalType;
+    private String color;
+    private int modelYear;
+    private double weight;
+    public boolean needsCharging = true;
+    private int batteryPercentage;
+
+    public boolean needsOil = false;
+    public int oilLevel = 5;
+
+    //TODO Create some kinds of methods that addresses hunger, thirst etc....
+
+    public VirtualRobotPet(String name, String animalType, String furColor, int modelYear, double weight) {
+        this.name = name;
+        this.animalType = animalType;
+        this.color = furColor;
+        this.modelYear = modelYear;
+        this.weight = weight;
+    }
+
+
+    public abstract String play();
+
+    public abstract String getOiled();
+    public abstract String ASCIIArt();
+    public abstract String talking();
+    public abstract String getLocation();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+
+
+
+
+    public boolean needsOil() {
+        if (oilLevel <= 3) {
+            System.out.println("You should consider giving it oil.");
+            needsOil = true;
+        }
+        return needsOil;
+    }
+
+    public String checkup() {
+        String petStats = "";
+        petStats += "Oil-level: " + oilLevel;
+        petStats+="Battery=level" + batteryPercentage;
+
+
+        return petStats;
+    }
+
+    public String greeting() {
+        String hello = this.ASCIIArt();
+        hello += "My name is " + name + ". ";
+        hello += "I am " + getModelYear() + " years old. ";
+        hello += "I am " + getWeight() + " lbs. ";
+        return hello;
+    }
+}
